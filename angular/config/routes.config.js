4836b74333e26e6f4f9e96c90ed86669b28e220a
Alain BERRIER <alain.berrier@outlook.com>
2017-04-14 14:50:44 +0200

    - add page Mes Amis

--- angular/config/routes.config.js
+++ angular/config/routes.config.js
@@ -1,70 +1,78 @@
 export function RoutesConfig($stateProvider, $urlRouterProvider) {
-	'ngInject';
+    'ngInject';
 
-	let getView = (viewName) => {
-		return `./views/app/pages/${viewName}/${viewName}.page.html`;
-	};
+    let getView = (viewName) => {
+        return `./views/app/pages/${viewName}/${viewName}.page.html`;
+    };
 
-	$urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
     /*
-        data: {auth: true} would require JWT auth
-        However you can't apply it to the abstract state
-        or landing state because you'll enter a redirect loop
-    */
+     data: {auth: true} would require JWT auth
+     However you can't apply it to the abstract state
+     or landing state because you'll enter a redirect loop
+     */
 
-	$stateProvider
-		.state('app', {
-			abstract: true,
-            data: {},
-			views: {
-				header: {
-					templateUrl: getView('header')
-				},
-				footer: {
-					templateUrl: getView('footer')
-				},
-				main: {}
-			}
-		})
-		.state('app.landing', {
-            url: '/',
-            views: {
-                'main@': {
-                    templateUrl: getView('landing')
+    $stateProvider
+            .state('app', {
+                abstract: true,
+                data: {},
+                views: {
+                    header: {
+                        templateUrl: getView('header')
+                    },
+                    footer: {
+                        templateUrl: getView('footer')
+                    },
+                    main: {}
                 }
-            }
-        })
-        .state('app.login', {
-			url: '/login',
-			views: {
-				'main@': {
-					templateUrl: getView('login')
-				}
-			}
-		})
-        .state('app.register', {
-            url: '/register',
-            views: {
-                'main@': {
-                    templateUrl: getView('register')
+            })
+            .state('app.landing', {
+                url: '/',
+                views: {
+                    'main@': {
+                        templateUrl: getView('landing')
+                    }
                 }
-            }
-        })
-        .state('app.forgot_password', {
-            url: '/forgot-password',
-            views: {
-                'main@': {
-                    templateUrl: getView('forgot-password')
+            })
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'main@': {
+                        templateUrl: getView('login')
+                    }
                 }
-            }
-        })
-        .state('app.reset_password', {
-            url: '/reset-password/:email/:token',
-            views: {
-                'main@': {
-                    templateUrl: getView('reset-password')
+            })
+            .state('app.register', {
+                url: '/register',
+                views: {
+                    'main@': {
+                        templateUrl: getView('register')
+                    }
                 }
-            }
-        });
+            })
+            .state('app.forgot_password', {
+                url: '/forgot-password',
+                views: {
+                    'main@': {
+                        templateUrl: getView('forgot-password')
+                    }
+                }
+            })
+            .state('app.reset_password', {
+                url: '/reset-password/:email/:token',
+                views: {
+                    'main@': {
+                        templateUrl: getView('reset-password')
+                    }
+                }
+            })
+            .state('app.friends', {
+                url: '/friends',
+                views: {
+                    'main@': {
+                        templateUrl: getView('friends')
+                    }
+                }
+            });
 }
