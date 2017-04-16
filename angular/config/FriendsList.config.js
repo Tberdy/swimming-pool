1a89b100f142d60383beeec7ad3d835ba79f20cf
Alain BERRIER <alain.berrier@outlook.com>
2017-04-16 17:38:47 +0200

    -modifications on friends page

---
+++ angular/config/FriendsList.config.js
@@ -0,0 +1,8 @@
+export function FriendsListConfig(/*$mdIconProvider*/) {
+    'ngInject';
+
+    //$mdIconProvider.iconSet('social', 'img/icons/sets/social-icons.svg', 24);
+    //$mdIconProvider.iconSet('device', 'img/icons/sets/device-icons.svg', 24);
+    //$mdIconProvider.iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
+    //$mdIconProvider.defaultIconSet('img/icons/sets/core-icons.svg', 24);
+}
