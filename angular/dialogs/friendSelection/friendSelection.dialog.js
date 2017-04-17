39f1c393f6e84069f9147239d102decf3b13b52e
Alain BERRIER <alain.berrier@outlook.com>
2017-04-17 16:36:32 +0200

    -friends : autocompletion

--- angular/dialogs/friendSelection/friendSelection.dialog.js
+++ angular/dialogs/friendSelection/friendSelection.dialog.js
@@ -1,16 +1,30 @@
-export class FriendSelectionController{
-    constructor(DialogService,parent,name){
+export class FriendSelectionController {
+    constructor(DialogService, parent, $event, $timeout, $q, $log) {
         'ngInject';
-        this.Dialog= DialogService;
-        this.parent=parent;
-        this.name=name;
+        this.Dialog = DialogService;
+        this.parent = parent;
+        this.event = $event;
+        this.timeout = $timeout;
+        this.q = $q;
+        this.log = $log;
+        this.simulateQuery = false;
+        this.isDisabled = false;
+
+        // list of `state` value/display objects
+        this.states = loadAll();
+        this.querySearch = querySearch;
+        this.selectedItemChange = selectedItemChange;
+        this.searchTextChange = searchTextChange;
+
+        this.newState = newState;
+
     }
-    save(){
+    save() {
         //Logic here
         this.Dialog.hide("wesh");
     }
 
-    cancel(){
+    cancel() {
         //alert('Cancel Button Pressed');
         this.Dialog.cancel("nope");
     }
