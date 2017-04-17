7aa115f3eec4e8b0ffb93ac2e2df54778e72b4df
Alain BERRIER <alain.berrier@outlook.com>
2017-04-17 12:51:00 +0200

    -add friends request

---
+++ tests/angular/app/components/friends-request.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test friends-request component',
+    modules: 'app',
+    element: '<friends-request></friends-request>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
