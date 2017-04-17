e503e5e582ab9081d62a1e4214d6ae029ab2ce05
Thomas Berdy <tberdy@hotmail.fr>
2017-04-17 12:26:39 +0200

    - friendship api (partial)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\User as User;
+use App\User as User;
 
 class UserController extends Controller {
 
@@ -19,31 +19,55 @@ class UserController extends Controller {
 
     public function update(Request $request) {
         $this->validate($request, [
-            'firstname'  => 'required|min:3',
-            'name'       => 'required|min:3',
-            'password'   => 'required|min:8',
-            'gender'     => 'required',
-            'birthdate'  => 'required'
+            'firstname' => 'required|min:3',
+            'name' => 'required|min:3',
+            'password' => 'required|min:8',
+            'gender' => 'required',
+            'birthdate' => 'required'
         ]);
-        
+
         $user = User::find($request->id);
-        
+
         $user->firstname = trim($request->firstname);
         $user->name = trim($request->name);
         $user->email = trim(strtolower($request->email));
         $user->password = bcrypt($request->password);
         $user->gender = intval($request->gender);
-        $user->birthdate = date( "Y-m-d", strtotime($request->birthdate));
-        
+        $user->birthdate = date("Y-m-d", strtotime($request->birthdate));
+
         $user->save();
-        
-        return response()->success();
+
+        return response()->success(array());
     }
 
     public function delete(Request $request) {
         User::destroy($request->id);
-        
-        return response()->success();
+
+        return response()->success(array());
+    }
+
+    public function listFriends(Request $request) {
+        $user = User::find($request->id);
+        $friends = $user->friendsIAdded()->get();
+        return response()->success(compact('friends'));
+    }
+
+    public function addFriend(Request $request) {
+        $user = User::find($request->id);
+        $user->friendsIAdded()->attach($request->friend_id);
+        return response()->success(array());
+    }
+
+    public function confirmFriend(Request $request) {
+        $user = User::find($request->id);
+        $user->friendsWhoAddMe()->attach($request->friend_id);
+        return response()->success(array());
+    }
+
+    public function deleteFriend(Request $request) {
+        $user = User::find($request->id);
+        $user->friendsIAdded()->detach($request->friend_id);
+        return response()->success(array());
     }
 
 }
