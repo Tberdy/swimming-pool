6176821eeabe23bc610a1ffdaf176683e9b88b28
Alain BERRIER <alain.berrier@outlook.com>
2017-04-19 17:53:06 +0200

    - news: creation of the news feed - correction of chat page - creation of Content Service

---
+++ tests/angular/services/ContentQuery.service.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test ContentQueryService',
+    modules: 'app',
+    inject: 'ContentQueryService',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
