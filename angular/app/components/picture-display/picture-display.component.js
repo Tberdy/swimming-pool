fc0dc20e976268ecb1353088aeec183c662d1787
Alain BERRIER <alain.berrier@outlook.com>
2017-04-21 03:13:42 +0200

    - photos : final changes

--- angular/app/components/picture-display/picture-display.component.js
+++ angular/app/components/picture-display/picture-display.component.js
@@ -1,12 +1,25 @@
 class PictureDisplayController{
-    constructor(CurrentUserService,ContentQueryService){
+    constructor(CurrentUserService,ContentQueryService,API){
         'ngInject';
         this.CurrentUser=CurrentUserService;
         this.ContentQuery=ContentQueryService;
-        //
+        this.API=API;
+        this.user=[];
+        this.data=[];
     }
 
     $onInit(){
+        let promiseUser = this.CurrentUser.getUserPromise();
+        promiseUser.then((response) => {
+            this.user = angular.copy(response);
+            this.API.all('content/list/pictures').get('', {
+                user_id: this.user.id
+            }).then((response) => {
+                this.data = angular.copy(response.data.pictures);
+                
+                
+            });
+        });
     }
 }
 
